ae9fff13868223624abaaac88f3dd208b7e97546
RAJANIRAI1985 <90604673+RAJANIRAI1985@users.noreply.github.com>
2021-09-13 16:02:29 +0530

    Update Inbox.test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,28 +5,3 @@ const Web3 = require('web3');  // Web3 is contructor function (class like packag
 //Set up local instance of web3 and use the provider of the network . 1 provider for 1 network
 const web3 = new Web3(ganache.provider);
 
-let car;
-
-beforeEach(() => {
-  //console.log('next car starting');
- car = New Car;
-});
-
-Class Car {
-  park{
-  return "stopped";
-  }
-  drive{
-  return "speed";
-  }
-}
-describe('car class test result i got',() => {
-  it('can has a park function',=> {
-     asset.equal(car.park(),"stopped");
-     });
-it('can has a drive function',=> {
-     asset.equal(car.drive(),"speed");
-     });
-});      
-
-
