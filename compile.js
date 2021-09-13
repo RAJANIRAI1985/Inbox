e6accbac6f0098a18928a108b12b81ebe9d9c5a3
RAJANIRAI1985 <90604673+RAJANIRAI1985@users.noreply.github.com>
2021-09-13 15:16:04 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -1,1 +1,7 @@
 // compile code will go here
+const path = require('path');
+const fs = require('fs');
+const sols = require('sols');
+const Inboxpath = path.resolve(_dirname,'contracts','Inbox.sol');
+const source = fs.readFileSynk(Iboxpath, 'utf8');
+console.log(solc.compile(source, 1));
