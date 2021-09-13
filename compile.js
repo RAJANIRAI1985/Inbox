1f6430d2401ffed5913d489fd5bbee05cef40d44
RAJANIRAI1985 <90604673+RAJANIRAI1985@users.noreply.github.com>
2021-09-13 15:22:00 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -4,4 +4,6 @@ const fs = require('fs');
 const sols = require('sols');
 const Inboxpath = path.resolve(_dirname,'contracts','Inbox.sol');
 const source = fs.readFileSynk(Inboxpath, 'utf8');
-console.log(solc.compile(source, 1));
+//console.log(solc.compile(source, 1));
+//we only care about contracts/inbox property in the utput of the compiled code
+module.export = solc.compile(source, 1).contracts[':/Inbox']; 
