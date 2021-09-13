d1e9689bc4c60a571c23e25923d25d3281964053
RAJANIRAI1985 <90604673+RAJANIRAI1985@users.noreply.github.com>
2021-09-13 15:18:13 +0530

    Update compile.js

--- compile.js
+++ compile.js
@@ -3,5 +3,5 @@ const path = require('path');
 const fs = require('fs');
 const sols = require('sols');
 const Inboxpath = path.resolve(_dirname,'contracts','Inbox.sol');
-const source = fs.readFileSynk(Iboxpath, 'utf8');
+const source = fs.readFileSynk(Inboxpath, 'utf8');
 console.log(solc.compile(source, 1));
